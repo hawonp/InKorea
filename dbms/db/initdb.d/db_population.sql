b1e7609e32c48927f794602e141d65277b4fdc06
Hawon Park <hawonp@gmail.com>
2022-04-21 10:06:22 +0900

    db population WIP

---
+++ dbms/db/initdb.d/db_population.sql
@@ -0,0 +1,4 @@
+-- Data Population for Categories / Subcategories
+INSERT INTO Category(category_id, category_name) VALUES(1, "Banking");
+INSERT INTO Category(category_id, category_name) VALUES(2, "");
+INSERT INTO Category(category_id, category_name) VALUES(3, "Health Insurance");
