cd8a88c02d02b1c098323f09fec050f60073565d
Hawon Park <hawonp@gmail.com>
2022-04-22 23:08:18 +0900

    edefwff

--- dbms/db/initdb.d/db_tables_population.sql
+++ dbms/db/initdb.d/db_tables_population.sql
@@ -8,10 +8,31 @@ INSERT INTO Category(category_name) VALUES("Mobile");
 INSERT INTO Subcategory(subcategory_name, category_id) VALUES("Creating a bank account", "1");
 INSERT INTO Subcategory(subcategory_name, category_id) VALUES("How to use an ATM", "1");
 
+-- Subcategory (Health)
+INSERT INTO Subcategory(subcategory_name, category_id) VALUES("How to get basic health insurance", "2");
+INSERT INTO Subcategory(subcategory_name, category_id) VALUES("How to upgrade your health insurance", "2");
 
--- Document helper related
--- INSERT INTO Document(document_title, has_details, subcategory_id) VALUES("Alien Registration Card (ARC)", "Yes", "0");
--- INSERT INTO Document(document_title, has_details, subcategory_id) VALUES("Passport / ID", "No", "0");
--- INSERT INTO Document(document_title, has_details, subcategory_id) VALUES("Passport / ID", "No", "0");
+-- Subcategory (Housing)
+INSERT INTO Subcategory(subcategory_name, category_id) VALUES("How to get a house (Jeonse)", "3");
+INSERT INTO Subcategory(subcategory_name, category_id) VALUES("How to get a house (Rent)", "3");
 
-SELECT * FROM Category;
+-- Subcategory (Mobile)
+INSERT INTO Subcategory(subcategory_name, category_id) VALUES("How to get basic health insurance", "4");
+INSERT INTO Subcategory(subcategory_name, category_id) VALUES("How to upgrade your health insurance", "4");
+
+-- Scenario Guide (Banking Specific)
+INSERT INTO Phrase(phrase_text, phrase_text_kor, phrase_audio_link) VALUES ("first phrase", "1번 문장", "random link");
+INSERT INTO Phrase(phrase_text, phrase_text_kor, phrase_audio_link) VALUES ("second phrase", "2번 문장", "random link");
+INSERT INTO Phrase(phrase_text, phrase_text_kor, phrase_audio_link) VALUES ("third phrase", "3번 문장", "random link");
+INSERT INTO Phrase(phrase_text, phrase_text_kor, phrase_audio_link) VALUES ("fourth phrase", "4번 문장", "random link");
+INSERT INTO Phrase(phrase_text, phrase_text_kor, phrase_audio_link) VALUES ("fifth phrase", "5번 문장", "random link");
+
+INSERT INTO Phrase_Start(subcategory_id, phrase_id) VALUES (1, 1);
+INSERT INTO Phrase_Link(from_id, to_id) VALUES (1, 2);
+INSERT INTO Phrase_Link(from_id, to_id) VALUES (2, 3);
+INSERT INTO Phrase_Link(from_id, to_id) VALUES (4, 5);
+
+-- Document Helper (Banking Specific)
+INSERT INTO Document(document_title, has_details, )
+
+SELECT * FROM Phrase_Link;
