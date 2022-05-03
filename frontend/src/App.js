1c8bf6c3316d58e3bdf63943502513e54348364e
Jeongho Shin <45681556+je0shin@users.noreply.github.com>
2022-05-03 21:33:54 +0900

    [WIP] search bar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,15 @@ import logo from "./logo.svg";
 import "./styles/App.css";
 import Button from "@mui/material/Button";
 
+import Sidebar from "./components/Sidebar";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <Button variant="contained">Hello World</Button>
+        <Sidebar />
       </header>
     </div>
   );
