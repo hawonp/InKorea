7668319fedf192bcce995c3b7e1d33cccdac23cd
Jeongho Shin <45681556+je0shin@users.noreply.github.com>
2022-05-05 12:32:49 +0900

    [WIP] react router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./styles/index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
@@ -7,7 +8,16 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          {/* <Route path="scenario" element={< />}></Route> */}
+          {/* <Route path="quiz" element={< />}></Route> */}
+          {/* <Route path="document" element={< />}></Route> */}
+          {/* <Route path="appcatalog" element={< />}></Route> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
