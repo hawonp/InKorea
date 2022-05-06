15e1df03b810f3836aa170f0bb294c567656ee6e
uprain1116 <74394318+uprain1116@users.noreply.github.com>
2022-05-07 00:32:51 +0900

    conversation component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,14 @@ import "./styles/App.css";
 import Button from "@mui/material/Button";
 import Heading from "./components/Heading";
 import ConversationLeft from "./components/ConversationLeft";
+import ConversationRight from "./components/ConversationRight";
 
 function App() {
   return (
     <>
         <Heading/>
         <ConversationLeft/>
+        <ConversationRight/>
     </>
 
   );
--- frontend/src/components/ConversationLeft.js
+++ frontend/src/components/ConversationLeft.js
@@ -1,15 +1,17 @@
 import "../styles/Conversation.css";
+import profileImage from "../assets/images/outline_profile.png";
+import soundImage from "../assets/images/outline_volume.png";
 
 function ConversationLeft(){
     return(
         <div id = "outerBox">
-            <div id = "speaker">image</div>
+            <div id = "speaker"><img src= {profileImage}/></div>
             <div id = "sentence">
-                <div class = "left" id = "english"> English</div>
-                <div class = "left" id = "korean"> 한글입니다</div>
+                <div class = "left" id = "english"> Hello. How can I help you? </div>
+                <div class = "left" id = "korean"> 안녕하세요. 어떻게 도와드릴까요? </div>
                 <div class = "left" id = "pronunciation"> 
-                    <div id = "text">text</div>
-                    <div id = "sound">sound</div>
+                    <div id = "text">Annyeonghaseyo. Eotteohge dowaderikkayo? </div>
+                    <div id = "sound"><img src= {soundImage} width = {"40"}/></div>
                 </div>
             </div>
             <div id = "other">optional</div>
