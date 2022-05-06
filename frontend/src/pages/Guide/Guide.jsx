24d121c348a804baec9774f508d242b5118daa2c
Jeongho Shin <45681556+je0shin@users.noreply.github.com>
2022-05-06 14:31:25 +0900

    [MIN] minor changes to sidebar

--- frontend/src/pages/Guide/Guide.jsx
+++ frontend/src/pages/Guide/Guide.jsx
@@ -9,10 +9,7 @@ import List from "@mui/material/List";
 import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
 import ListItem from "@mui/material/ListItem";
-import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
-import InboxIcon from "@mui/icons-material/MoveToInbox";
-import MailIcon from "@mui/icons-material/Mail";
 import MainAppBar from "../../components/MainAppBar/MainAppBar";
 
 const drawerWidth = 240;
@@ -26,11 +23,8 @@ export default function Guide(props) {
       <Toolbar />
       <Divider />
       <List>
-        {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
+        {["One", "Two", "Three", "Four"].map((text, index) => (
           <ListItem button key={text}>
-            <ListItemIcon>
-              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-            </ListItemIcon>
             <ListItemText primary={text} />
           </ListItem>
         ))}
