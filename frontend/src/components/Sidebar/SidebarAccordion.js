ec043c5b480a4dc9e3e3e1d1d63dcaf9e50d8f7c
Jeongho Shin <45681556+je0shin@users.noreply.github.com>
2022-05-13 14:30:57 +0900

    [WIP] accordion

---
+++ frontend/src/components/Sidebar/SidebarAccordion.js
@@ -0,0 +1,31 @@
+import * as React from "react";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+export default function SidebarAccordion() {
+  return (
+    <div>
+      {["One", "Two", "Three", "Four"].map((text, index) => (
+        <Accordion>
+          <AccordionSummary
+            expandIcon={<ExpandMoreIcon />}
+            aria-controls="panel2a-content"
+            id="panel2a-header"
+          >
+            <Typography>{text}</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
+              eget.
+            </Typography>
+          </AccordionDetails>
+        </Accordion>
+      ))}
+    </div>
+  );
+}
