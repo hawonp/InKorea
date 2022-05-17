56476837d5751a6e16e9dbb69939e2f18b0f16ed
Hawon Park <hawonp@gmail.com>
2022-05-17 20:49:52 +0900

    Starting on Scenario

---
+++ frontend/src/components/Guide/ScenarioGuide.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Stack from "@mui/material/Stack";
+import { styled } from "@mui/material/styles";
+
+import axiosInstance from "../../utils/routeUtils";
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
+
+export default function ScenarioGuide({ data }) {
+  useEffect(() => {
+      axiosInstance.get()
+  });
+  return (
+    <div>
+      <Box sx={{ width: "100%" }}>
+        <Stack spacing={2}>
+          <Item>Item 1</Item>
+          <Item>Item 2</Item>
+          <Item>Item 3</Item>
+        </Stack>
+      </Box>
+    </div>
+  );
+}
