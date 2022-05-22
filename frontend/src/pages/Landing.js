466d003346a99dc3bbd30f07726b02c3851b33ed
Jeongho Shin <45681556+je0shin@users.noreply.github.com>
2022-05-23 01:07:05 +0900

    [WIP] landing page

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -1,35 +1,110 @@
-import React from "react";
-import { Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { Box, Card } from "@mui/material";
+import Zoom from "@mui/material/Zoom";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import { CardMedia, Typography } from "@mui/material";
 import {
   createTheme,
   responsiveFontSizes,
   ThemeProvider,
+  useTheme,
 } from "@mui/material/styles";
-
+import guideImage from "../assets/images/guide.jpg";
+import appsImage from "../assets/images/apps.jpg";
 import MainAppBar from "../components/MainAppBar/MainAppBar";
+import { experimentalStyled as styled } from "@mui/material/styles";
 
 let theme = createTheme();
 theme = responsiveFontSizes(theme);
 
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(2),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
+
 export default function Landing() {
-  theme.typography.h3 = {
-    fontSize: "1.2rem",
-    "@media (min-width:600px)": {
-      fontSize: "1.5rem",
-    },
-    [theme.breakpoints.up("md")]: {
-      fontSize: "2.4rem",
-    },
-  };
+  const [checked, setChecked] = useState(false);
+
+  useEffect(() => {
+    setChecked(true);
+  }, []);
 
   return (
-    <div>
+    <Box>
       <MainAppBar />
-      <div>
+      <Box sx={{ mt: 10 }}>
         <ThemeProvider theme={theme}>
-          <Typography variant="h2">Welcome to InKorea</Typography>
+          <Zoom
+            in={checked}
+            timeout={700}
+            style={{ transitionDelay: checked ? "500ms" : "100ms" }}
+          >
+            <Typography variant="h2" align="center">
+              Welcome to InKorea
+            </Typography>
+          </Zoom>
         </ThemeProvider>
-      </div>
-    </div>
+      </Box>
+      <Grid
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+        container
+        // spacing={{ xs: 2, md: 3 }}
+        columns={{ xs: 4, sm: 8, md: 12 }}
+      >
+        <Item elevation={0}>
+          <Card sx={{ minWidth: 275, maxWidth: 600 }}>
+            <CardMedia
+              component="img"
+              height="194"
+              image={guideImage}
+              alt="Scenario"
+            />
+          </Card>
+        </Item>
+        <Item elevation={0}>
+          <Card sx={{ minWidth: 275, maxWidth: 600 }}>
+            <CardMedia
+              component="img"
+              height="194"
+              src={appsImage}
+              alt="App catalog"
+            />
+          </Card>
+        </Item>
+        {/* <Item>
+          <Card>
+            <div style={{ position: "relative" }}>
+              <CardMedia
+                style={{ height: "250px", paddingTop: "2%" }}
+                component="img"
+                src={appsImage}
+                title="Pancakes"
+                alt="Pancakes"
+              />
+              <div
+                style={{
+                  position: "absolute",
+                  color: "white",
+                  top: 10,
+                  left: "50%",
+                  transform: "translateX(-50%)",
+                }}
+              >
+                {" "}
+                Some text
+              </div>
+            </div>
+          </Card>
+        </Item> */}
+      </Grid>
+    </Box>
   );
 }
