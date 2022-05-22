47b246671fd5e92d92fee423b9f48aba0498f2e0
Young Won Choi <ywckok@gmail.com>
2022-05-22 22:39:31 +0900

    Changed AppBox layout to Card

--- frontend/src/pages/Appcatalog/AppCatalog.jsx
+++ frontend/src/pages/Appcatalog/AppCatalog.jsx
@@ -1,12 +1,20 @@
 import MainAppBar from "../../components/MainAppBar/MainAppBar";
 import CatalogGrid from "../../components/AppCatalog/CatalogGrid";
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
 
 export default function AppCatalog() {
   return (
     <div>
       <MainAppBar />
-      <h2> App catalog page</h2>
-        <CatalogGrid/>
+      <h2 align={"center"}> App Catalog</h2>
+        <Box justifySelf={"center"}>
+        <TextField id="standard-basic" label="Search" variant="standard" />
+        </Box>
+        <Container maxWidth={"xl"}>
+            <CatalogGrid/>
+        </Container>
     </div>
   );
 }
